fafe66f168e768ab4aa7da534a204571ff025894
Ashutosh <cyberboy9971@gmail.com>
2023-10-09 18:23:43 +0530

    Simple put and get tested

--- main.rs
+++ main.rs
@@ -1,13 +1,18 @@
-use rocksdb::{DB, Options, WriteBatch, WriteOptions};
+use rocksdb::{DB};
 
-let path = "/home/hashkat/Documents/sdslabs/VectorDB/rocksdb";
-
-{
+fn main(){
+    let path = "/home/hashkat/Documents/sdslabs/VectorDB/rocksdb";
     let db = DB::open_default(path).unwrap();
     db.put(b"Some random key", b"Some random value").unwrap();
 
     match db.get(b"Some random key") {
-        Ok(Some(value)) => println!("retrieved value {}", value.to_utf8().unwrap()),
+        Ok(Some(value)) => {
+            if let Ok(value_as_str) = std::str::from_utf8(&value) {
+                println!("retrieved value {}", value_as_str);
+            } else {
+                println!("Invalid UTF-8 data");
+            }
+        },
         Ok(None) => println!("value not found"),
         Err(e) => println!("operational problem encountered: {}", e),
     }
